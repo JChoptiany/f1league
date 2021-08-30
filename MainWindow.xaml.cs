8308f763e6d7d25080168e84256c75d097b2c889
JChoptiany <jakub.choptiany1@gmail.com>
2021-08-30 16:15:18 +0200

    first commit

---
+++ MainWindow.xaml.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Windows;
+using System.Windows.Data;
+using System.Data.SqlClient;
+
+namespace formula1
+{
+    public partial class MainWindow : Window
+    {
+        private static string connectionString = @"Data Source=(LocalDB)\MSSQLLocalDB;AttachDbFilename=C:\Users\jakub\source\repos\formula1\Database.mdf;Integrated Security=True";
+        
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void IndividualRadioButton_Checked(object sender, RoutedEventArgs e)
+        {
+            TableTitleLabel.Content = "Klasyfikacja indywidualna";
+        }
+
+        private void TeamRadioButton_Checked(object sender, RoutedEventArgs e)
+        {
+            TableTitleLabel.Content = "Klasyfikacja drużynowa";
+        }
+    }
+}
